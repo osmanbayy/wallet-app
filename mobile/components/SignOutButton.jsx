cd5c1c19bad73e2d02163f60a8b12cdda7564932
Osman Bay <bayosman342@gmail.com>
2025-12-12 01:39:01 +0300

    chore: update dependencies in mobile package.json
    - Added @clerk/clerk-expo version 2.19.11
    - Added expo-secure-store version 15.0.8
    - Moved react-native-worklets to the end of the dependencies list
    - Ensured typescript version 5.9.2 is included in devDependencies

---
+++ mobile/components/SignOutButton.jsx
@@ -0,0 +1,21 @@
+import { useClerk } from "@clerk/clerk-expo";
+import { Text, TouchableOpacity } from "react-native";
+
+export const SignOutButton = () => {
+  // Use `useClerk()` to access the `signOut()` function
+  const { signOut } = useClerk();
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+  return (
+    <TouchableOpacity onPress={handleSignOut}>
+      <Text>Sign out</Text>
+    </TouchableOpacity>
+  );
+};
