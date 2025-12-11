1656d53cd3e1fd035c7193a36c42edd4d21df1a2
Osman Bay <bayosman342@gmail.com>
2025-12-11 17:12:09 +0300

    initialize commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express"
+import "dotenv/config"
+import { sql } from "./config/db.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10, 2) NOT NULL,
+      category VARCHAR NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialized successfully!");
+  } catch (error) {
+    console.log("Error initializing database ", error);
+    process.exit(1) // status code 1 means failure, 0 is success
+  }
+}
+
+app.get("/", (req, res) => {
+  res.send("It's working");
+})
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+  })
+})
