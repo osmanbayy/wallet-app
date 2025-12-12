3162479cfdea5132a5f62c0b2fc954af81a0e720
Osman Bay <bayosman342@gmail.com>
2025-12-12 18:47:20 +0300

    feat(cron): add cron job to send GET request every 14 minutes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,11 @@ import "dotenv/config"
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js"
+import job from "./config/cron.js"
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start()
 
 // Middleware
 app.use(express.json());
@@ -13,6 +16,10 @@ app.use(rateLimiter)
 const PORT = process.env.PORT || 5001;
 
 app.use("/api/transactions", transactionsRoute);
+
+app.get("/api/health", (request, response) => {
+  response.status(200).json({ status: "ok" })
+})
 
 initDB().then(() => {
   app.listen(PORT, () => {
